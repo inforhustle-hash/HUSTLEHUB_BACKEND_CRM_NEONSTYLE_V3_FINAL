978225c4c7519ee2ee10f7f6c01627f04695e557
inforhustle-hash <inforhustle@gmail.com>
2025-12-13 19:18:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Error:", err));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("HustleHub Backend Running");
+});
+
+// REQUIRED for Render
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
