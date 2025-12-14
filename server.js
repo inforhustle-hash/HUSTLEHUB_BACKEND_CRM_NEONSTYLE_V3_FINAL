65fb0225a38e85aec954c61bb4d573c09abdd341
inforhustle-hash <inforhustle@gmail.com>
2025-12-13 19:14:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
